35ecb065e881effe0273de6885b4807feb97e468
Nikola <matviivcareer@gmail.com>
2020-08-05 12:07:12 +0300

    user service skeleton

--- izzitest/izzitest/settings.py
+++ izzitest/izzitest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'order',
     'user',
+    'user_service',
 ]
 
 MIDDLEWARE = [
--- izzitest/user_service/models.py
+++ izzitest/user_service/models.py
@@ -1,3 +1,61 @@
-from django.db import models
+"""
+Service for work with user
+"""
+from user.models import User
+from utils.csv_parser import CsvParser
 
-# Create your models here.
+
+class UserService():
+    """
+    Class for work with user
+    """
+    # positions in csv row
+    NAME = 0
+    SURNAME = 1
+    BIRTHDAY = 2
+    REGISTRATION_DATE = 3
+
+    @staticmethod
+    def add_user(name, surname, birthday, registration_date):
+        """
+        prepare data and create user
+
+        :param name: str
+        :param surname: str
+        :param birthday: str | date
+        :param registration_date: str | date
+        :return: User object or None
+        """
+
+        birthday = User.format_date(birthday)
+        registration_date = User.format_date(registration_date)
+        user = User.create(
+            name=name,
+            surname=surname,
+            birthday=birthday,
+            registration_date=registration_date
+        )
+        return user
+
+    @staticmethod
+    def add_users_from_csv(file):
+        """
+        Add users from csv file
+
+        :param file: str | file path
+        :return:
+        """
+
+        start = 0
+        rows = CsvParser.parse(file)
+
+        if CsvParser.is_heading(rows[0]):
+            start = 1
+
+        for row in rows[start:]:
+            UserService.add_user(
+                name=row[UserService.NAME],
+                surname=row[UserService.SURNAME],
+                birthday=row[UserService.BIRTHDAY],
+                registration_date=row[UserService.REGISTRATION_DATE]
+            )
