878f2954c3ec4264faf788e66c32a0402215228f
Nikola <matviivcareer@gmail.com>
2020-08-05 17:33:36 +0300

    name refactor

--- izzitest/user/admin.py
+++ izzitest/user/admin.py
@@ -1,5 +1,5 @@
 """
-Admin
+Add user from csv on admin page
 """
 import os
 
@@ -8,17 +8,26 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect
 from django.urls import path
 
-from .models import User
 from user_service.models import UserService
+from .models import User
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    FILEPATH = 'media/users.csv'
+    """
+    Custom user admin page
+    """
+    FILEPATH = 'media/users.csv'  # path where file with users is stored
     list_display = ('name', 'surname', 'birthday', 'registration_date')
     ordering = ('name',)
     change_list_template = 'admin/user/change_form.html'
 
     def get_urls(self):
+        """
+        Url for custom user model
+
+        :return: str
+        """
         urls = super().get_urls()
         custom_urls = [
             path('add_users', self.add_users)
@@ -26,8 +35,15 @@ class UserAdmin(admin.ModelAdmin):
 
         return custom_urls + urls
 
+    # pylint: disable=no-self-use
     def add_users(self, request):
-        users = request.FILES.get('users')
+        """
+        Save uploaded file and add users from it
+
+        :param request:
+        :return: Http response
+        """
+        users = request.FILES.get('users')  # get file from request
 
         if not users:
             messages.warning(request, 'NO FILE')
@@ -37,11 +53,13 @@ class UserAdmin(admin.ModelAdmin):
             messages.warning(request, 'WRONG TYPE')
             return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
-        fs = FileSystemStorage()
-        if os.path.exists('media/users.csv'):
+        file_system = FileSystemStorage()
+
+        if os.path.exists('media/users.csv'):  # if such file already exist
             os.remove('media/users.csv')
-        fs.save('users.csv', users)  # use users.name if u want to save file origin name
+        file_system.save('users.csv', users)  # use users.name if u want to save file origin name
 
         UserService.add_users_from_csv(UserAdmin.FILEPATH)
         messages.info(request, 'Users Added')
+
         return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
